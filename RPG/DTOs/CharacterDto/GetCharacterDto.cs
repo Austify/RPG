a4124a3300a637e6f420d2583dd4db00700943c4
dimirage <eroataga@gmail.com>
2021-11-25 20:47:50 +0100

    First commit

---
+++ RPG/DTOs/CharacterDto/GetCharacterDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using RPG.Models;
+
+namespace RPG.DTOs.CharacterDto
+{
+    public class GetCharacterDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "Frodo";
+
+        public int HitPoints { get; set; } = 100;
+
+        public int Strength { get; set; } = 10;
+
+        public int Defense { get; set; } = 10;
+
+        public int Intelligence { get; set; } = 10;
+
+        public RpgClass RpgClass { get; set; } = RpgClass.Knight;
+    }
+}
