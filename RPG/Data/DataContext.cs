2200fc2ea3c817ac0a2323ef6aff5164043d267b
dimirage <eroataga@gmail.com>
2021-11-26 06:59:41 +0100

    Added Context file

---
+++ RPG/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using RPG.Models;
+
+namespace RPG.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Character> Characters { get; set; }
+    }
+}
